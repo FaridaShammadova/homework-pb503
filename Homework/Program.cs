9b97c901d00f7a58f20115bda9d0aa0bebf67e01
Farida R. Shammadova <faridarsh@code.local>
2024-10-19 13:58:27 +0400

    added Homework directory and homework.png picture

---
+++ Homework/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int i = 2;
+
+            //for (i = 2; i <= number; i++)
+            //{
+            //    if (number % i == 0)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //if (number == 1)
+            //{
+            //    Console.WriteLine("Bu eded ne sade, ne de murekkeb ededdir!");
+            //}
+            //else if (number == i)
+            //{
+            //    Console.WriteLine("Bu eded sade ededdir!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Bu eded murekkeb ededdir!");
+            //}
+            #endregion
+
+            #region Task2
+            int number2 =Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            if(number2 == 0)
+            {
+                s = 1;
+            }
+            while (number2 > 0)
+            {
+                number2 /= 10;
+                s++;
+            }
+            Console.WriteLine(s);
+            #endregion
+
+        }
+    }
+}
